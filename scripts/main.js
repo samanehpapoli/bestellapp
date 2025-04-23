699b36b96228ece51eb3b10d614234cfb570c450
Samaneh Papoli <faalsamaneh@gmail.com>
2025-04-23 17:28:40 +0200

    Implement several new functions for data processing

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
+const DELIVERY_COST = 4.99;
+let baskets = [];
 function init() {
   renederCategory();
   renderMenuCategory();
@@ -6,11 +8,10 @@ function init() {
 function renderMenuCategory() {
   let MenuCategoryElement = document.getElementById("menu-category");
   MenuCategoryElement.innerHTML = "";
-  for (let i = 0; i <categories.length; i++) {
-    MenuCategoryElement.innerHTML += getMenuCategoryTemplate(i);
+  for (let i = 0; i < categories.length; i++) {
+    MenuCategoryElement.innerHTML += getMenuTemplate(i);
   }
 }
-
 
 function renederCategory() {
   let categoryElement = document.getElementById("category");
@@ -31,4 +32,63 @@ function renderFoods(index) {
       j
     );
   }
+}
+
+function addToBasket(categoryIndex, foodIndex) {
+  let food = categories[categoryIndex].foods[foodIndex];
+  let findBasketIndex = baskets.findIndex(
+    (basket) => basket.name === food.name
+  );
+  if (findBasketIndex >= 0) {
+    baskets[findBasketIndex].count++;
+  } else {
+    baskets.push({
+      name: food.name,
+      price: food.price,
+      count: 1,
+    });
+  }
+  renedrBasket();
+}
+
+function increaseBasket(index) {
+  baskets[index].count++;
+  renedrBasket();
+}
+
+function decreaseBasket(index) {
+  if (baskets[index].count === 1) {
+    deleteFromBasket(index);
+  } else {
+    baskets[index].count--;
+  }
+
+  renedrBasket();
+}
+
+function deleteFromBasket(index) {
+  baskets.splice(index, 1);
+  renedrBasket();
+}
+
+function renedrBasket() {
+  let basketFoodsElement = document.getElementById("basket-foods");
+  basketFoodsElement.innerHTML = "";
+  for (let i = 0; i < baskets.length; i++) {
+    basketFoodsElement.innerHTML += getBasketTemplate(i);
+  }
+  calculateSum();
+}
+
+function calculateSum() {
+  let sumPrice = 0;
+  for (let i = 0; i < baskets.length; i++) {
+    sumPrice += baskets[i].count * baskets[i].price;
+  }
+  sumPrice = parseFloat(sumPrice.toFixed(2));
+  document.getElementById("sumPrice").innerHTML = sumPrice;
+  document.getElementById("deliveryCost").innerHTML = DELIVERY_COST;
+  document.getElementById("totalPrice").innerHTML = (
+    sumPrice + DELIVERY_COST
+  ).toFixed(2);
 }
--- scripts/template.js
+++ scripts/template.js
@@ -1,21 +1,41 @@
 
-function getMenuCategoryTemplate(index){
-    return `<a href="#food-category-${index}">${categories[index].name}</a>`
+function getMenuTemplate(categoryIndex){
+    return `<a href="#food-category-${categoryIndex}">${categories[categoryIndex].name}</a>`
    }
 
 function getCategoryTemplate(index) {
+  let category = categories[index] 
   return ` <div class="food-category" id="food-category-${index}">
-                <img src="./asset/img/${categories[index].image}" alt="">
-                <h2>${categories[index].name}</h2>
+                <img src="./asset/img/${category.image}" alt="">
+                <h2>${category.name}</h2>
                 </div>
                 <div class="foods" id="foods${index}"></div>`;
 }
 
 function getFoodTemplate(categoryIndex, foodIndex) {
+  let food = categories[categoryIndex].foods[foodIndex]
   return ` <div class="food">
-                <h2>${categories[categoryIndex].foods[foodIndex].name}</h2>
-                <p>${categories[categoryIndex].foods[foodIndex].description}</p>
-                <span class="euro">${categories[categoryIndex].foods[foodIndex].price}</span>
-                <div class="icon icon-plus"></div>
+                <h2>${food.name}</h2>
+                <p>${food.description}</p>
+                <span class="euro">${food.price}</span>
+                <div class="icon icon-plus" onclick="addToBasket(${categoryIndex},${foodIndex})"></div>
             </div> `;
+}
+
+function getBasketTemplate(basketIndex) {
+  let food = baskets[basketIndex];
+  let totalPrice = (food.price * food.count).toFixed(2);
+  return  ` <div class="basket-food">
+            <h3>${food.name}</h3>
+            <div class="basket-food-info">
+              <div class="count">
+                <span class="icon icon-minus" onclick="decreaseBasket(${basketIndex})"></span>
+                <span>${food.count}x</span>
+                <span class="icon icon-plus" onclick="increaseBasket(${basketIndex})" ></span>
+              </div>
+              <div class="price euro">${totalPrice}</div>
+              <div class="icon icon-trash-o" onclick="deleteFromBasket(${basketIndex})"></div>
+            </div>
+          </div>`
+        
 }
